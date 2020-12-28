049d6db3714d7f7bab0a8b96e85b67ba446bf983
BurhanAique <burhanatique@gmail.com>
2020-12-28 15:44:41 +0500

    My first commit
    Uploaded all files

---
+++ index.js
@@ -0,0 +1,62 @@
+$(document).ready(function(e){
+    $win = $(window);
+    $navbar = $('#header');
+    $toggle = $('.toggle-button');
+    var width = $navbar.width();
+    toggle_onclick($win, $navbar, width);
+    // resize event
+    $win.resize(function(){
+        toggle_onclick($win, $navbar, width);
+    });
+
+    $toggle.click(function(e){
+        $navbar.toggleClass("toggle-left");
+    })
+
+});
+
+function toggle_onclick($win, $navbar, width){
+    if($win.width() <= 768){
+        $navbar.css({left: `-${width}px`});
+    }else{
+        $navbar.css({left: '0px'});
+    }
+}
+
+var typed = new Typed('#typed' , {
+    strings: [
+        'Associate Software Engineer @ i2c Inc' ,
+        'Web Developer',
+        'Machine Learning Enthusiast',
+        'Data Science Enthusiast',
+        'AI Enthusiast'
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
+
+var typed_2 = new Typed('#typed_2' , {
+    strings: [
+        'Associate Software Engineer @ i2c Inc' ,
+        'Web Developer',
+        'Machine Learning Enthusiast',
+        'Data Science Enthusiast',
+		'AI Enthusiast'
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor =>{
+    anchor.addEventListener('click', function(e){
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
